d7277765dfd6f50412425718359b0ec56572e017
Misha <selmisha10@gmail.com>
2022-07-26 18:34:50 +0600

    create second first and second section

---
+++ portfolio/dist/js/script.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', () => {
+   //!Menu
+   function openMenu(selectorMenu, activeClass) {
+      const menu = document.querySelector(selectorMenu);
+      menu.classList.add(activeClass);
+      document.body.style.overflow = 'hidden';
+   }
+   function clsoeMenu(selectorMenu, activeClass) {
+      const menu = document.querySelector(selectorMenu);
+      menu.classList.remove(activeClass);
+      document.body.style.overflow = '';
+   }
+
+
+   function menu(selectorBtnBurger, selectorMenu, selectorCloseMenu, activeClass) {
+      const targetMenuBtn = document.querySelector(selectorBtnBurger),
+         menu = document.querySelector(selectorMenu),
+         btnCloseMenu = document.querySelector(selectorCloseMenu);
+
+      targetMenuBtn.addEventListener('click', () => {
+         openMenu(selectorMenu, activeClass);
+      });
+
+      btnCloseMenu.addEventListener('click', () => {
+         clsoeMenu(selectorMenu, activeClass);
+      });
+
+      menu.addEventListener('click', (e) => {
+         const target = e.target;
+
+         if (target && target.classList.contains('menu__overlay')) {
+            clsoeMenu(selectorMenu, activeClass)
+         }
+      });
+
+      document.addEventListener('keydown', event => {
+         if (event.code === 'Escape') clsoeMenu(selectorMenu, activeClass);;
+      });
+
+   }
+
+   menu('.humburger', '.menu', '.menu__close', 'active');
+
+})
