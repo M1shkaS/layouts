2f8633774a36592392054c837edf8ecf146a1fe7
Misha <selmisha10@gmail.com>
2022-08-09 12:31:03 +0600

    adaptive prengi

--- prengi/gulpfile.js
+++ prengi/gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('server', function () {
 
 gulp.task('styles', function () {
    return gulp.src("src/sass/**/*.+(scss|sass)")
-      .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+      .pipe(sass({ outputStyle: 'nested' }).on('error', sass.logError))
       .pipe(rename({ suffix: '.min', prefix: '' }))
       .pipe(autoprefixer())
       .pipe(cleanCSS({ compatibility: 'ie8' }))
